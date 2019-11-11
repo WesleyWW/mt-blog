cfc5ead37eecb9b5ec173cc33ea4865f6b09182b
WWyane <wesleywyane@gmail.com>
2019-11-10 21:22:05 -0800

    123

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(express.json());
 
 const uri = require('./config/keys').mongoURI;
 
-mongoose.connect(process.env.ATLAS_URI || uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGODB_URI || uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true })
     .then(() => console.log('MongoDB connection established'))
     .catch(err => console.log(err));
 
@@ -29,7 +29,7 @@ if(process.env.NODE_ENV === 'production') {
     app.use(express.static('client/build'));
 
     app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     });
 }
 
