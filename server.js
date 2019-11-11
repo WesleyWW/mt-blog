41e18363cc5918af9fa0aa1216436a09492eba21
WWyane <wesleywyane@gmail.com>
2019-11-11 15:33:39 -0800

    123

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-app.use('/', express.static(path.join(__dirname, 'public')));
+app.use('/', express.static(path.join(__dirname, 'client/build')));
 
 const uri = require('./config/keys').mongoURI;
 
@@ -24,14 +24,14 @@ const blogRouter = require('./routes/blog');
 
 app.use('/blog', blogRouter);
 
-// if(process.env.NODE_ENV === 'production') {
+if(process.env.NODE_ENV === 'production') {
     //set static folder
     app.use(express.static('client/build'));
 
     app.get('*', (req, res) => {
         res.sendFile(path.join(__dirname, 'client/build/index.html'));
     });
-// }
+}
 
 app.listen(port, () => {
     console.log(`App listening on port ${port}!`);
