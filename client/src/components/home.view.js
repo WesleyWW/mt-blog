01f2be825686f2b7892b05c983ca1ace835977a8
WWyane <wesleywyane@gmail.com>
2019-11-11 16:06:04 -0800

    2e2

--- client/src/components/home.view.js
+++ client/src/components/home.view.js
@@ -64,7 +64,7 @@ export default class Home extends Component {
                 <section className="featured-blog-section">
                     <Featured />
                     <EmailForm />
-                    <Topics />
+                    {/* <Topics /> */}
                 </section>
             </div>
         )
